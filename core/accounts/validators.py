263d75849c1448753bd1abc8f6e19ca382a08548
arzdex <arzdex@gmail.com>
2024-08-14 10:44:05 +0330

    phone-validation for project number 0

--- core/accounts/validators.py
+++ core/accounts/validators.py
@@ -2,6 +2,14 @@ from django.core.exceptions import ValidationError
 import re
 
 def validate_iranian_cellphone_number(value):
-    pattern = r'^09\d{9}$'
-    if not re.match(pattern, value):
+    pattern_local = r'^09\d{9}$'
+    pattern_international = r'^\+989\d{9}$'
+
+    if not re.match(pattern_local, value) and not re.match(pattern_international, value):
         raise ValidationError('Enter a valid Iranian cellphone number.')
+
+    if re.match(pattern_local, value):
+        return
+
+    if re.match(pattern_international, value):
+        return
